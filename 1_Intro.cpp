b322447b3d8f9a55a64b8c1e806153b89610ed7b
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-06-17 14:01:37 +0530

    Add files via upload

---
+++ 1_Intro.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int x = 3;
+int y =6;
+
+// logical AND
+cout<<(x&y)<<endl;
+
+// OR OPERATOR
+cout<<(x||y)<<endl;
+
+// xor operator
+cout<<(x^y)<<endl;
+
+
+return 0;
+}
